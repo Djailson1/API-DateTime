1ec4115e5b8b768b5987e7fff10259d1acac5f2d
Djailson1 <djailson_silva1@outlook.com>
2022-06-27 21:09:53 -0300

    Formatando datas com o SimpleDateFormat

--- modulo_datas/src/modulo_datas/DatasEmJava.java
+++ modulo_datas/src/modulo_datas/DatasEmJava.java
@@ -1,18 +1,41 @@
 package modulo_datas;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DatasEmJava {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		
 		Date date = new Date();
 		
 		System.out.println("Dia do mês: " + date.getDate());
+		
 		System.out.println("Dia da semana: " + date.getDay());
+		
 		System.out.println("Hora do dia: " + date.getHours());
+		
 		System.out.println("Minuto da hora: " + date.getMinutes());
+		
 		System.out.println("Segundos: " + date.getSeconds());
+		
 		System.out.println("Ano: " + (date.getYear() + 1900));
+		
+		/*---------------Simple Date Format-------------*/
+		
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm.ss");
+		
+		System.out.println("Data atual em formato padrão e string: " + simpleDateFormat.format(date));
+		
+		simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm.ss");
+		
+		System.out.println("Data em formato para banco de dados: " + simpleDateFormat.format(date));
+		
+		simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		System.out.println("Objeto date: " + simpleDateFormat.parse("1987-10-18 "));/*convertendo string em objeto de data*/
+		
+		simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		System.out.println("Objeto date: " + simpleDateFormat.parse("18/10/1995 "));
 	}
 
 }
