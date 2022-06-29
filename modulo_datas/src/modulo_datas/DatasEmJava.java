a85712c735fb75271f1075d7e7a28850a32815a5
Djailson1 <djailson_silva1@outlook.com>
2022-06-28 22:04:43 -0300

    Gerando parcelas mensais e suas datas de vencimento

--- modulo_datas/src/modulo_datas/DatasEmJava.java
+++ modulo_datas/src/modulo_datas/DatasEmJava.java
@@ -11,10 +11,19 @@ import java.util.Date;
 public class DatasEmJava {
 	public static void main(String[] args) throws ParseException {
 		
+		Date dateInicial = new SimpleDateFormat("dd/MM/yyyy").parse("28/06/2022");
 		
-		long dias = ChronoUnit.DAYS.between(LocalDate.parse("2022-06-01"), LocalDate.now());/*total de dias de uma data até hoje*/
-		
-		System.out.println("Possui " + dias + " dias entre a faixa de data");
+				Calendar calendar = Calendar.getInstance();
+				
+				calendar.setTime(dateInicial);
+				
+				for(int parcela = 1; parcela <= 12; parcela++) {
+					
+					calendar.add(Calendar.MONTH, 1);
+					
+					System.out.println("Parcela número: " + parcela + " vencimento é em: " + new SimpleDateFormat("dd/MM/yyyy").format(calendar.getTime()));
+					
+				}
 		
 	}
 
