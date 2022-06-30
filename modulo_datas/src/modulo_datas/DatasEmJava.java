1bb00b43f2094568c808492f973fcc07afbde0d0
Djailson1 <djailson_silva1@outlook.com>
2022-06-29 22:41:05 -0300

    Como descobrir o tempo de processo com a classe Instant

--- modulo_datas/src/modulo_datas/DatasEmJava.java
+++ modulo_datas/src/modulo_datas/DatasEmJava.java
@@ -2,6 +2,8 @@ package modulo_datas;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -11,17 +13,23 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class DatasEmJava {
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args) throws ParseException, InterruptedException {
+		/*para sabermos quanto tempo demora um processo a ser executado*/
+		Instant inicio = Instant.now();
 		
-		LocalDate localDate = LocalDate.now();
-		System.out.println("Data atual: " + localDate.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+		Thread.sleep(2000);/*pode ser um processo com tempo qualquer que não conhecemos*/
 		
-		System.out.println("Dia da semana: " + localDate.getDayOfWeek().name());
-		System.out.println("Dia do mês: " + localDate.getDayOfMonth());
-		System.out.println("Dia do ano: " + localDate.getDayOfYear());
-		System.out.println("Mês: " + localDate.getMonth());
-		System.out.println("Ano: " + localDate.getYear());
+		Instant ifinal = Instant.now();
 		
+		Duration duration = Duration.between(inicio, ifinal);
+		
+		System.out.println("Duração em nanosegundos: " + duration.toNanos());
+		
+		System.out.println("Duração em minutos: " + duration.toMinutes());
+		
+		System.out.println("Duração em horas: " + duration.toHours());
+		
+		System.out.println("Duração em milisegundos: " + duration.toMillis());
 		
 		
 		
