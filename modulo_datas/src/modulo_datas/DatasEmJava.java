39a61f9cb57db4aa6e0668692f3e646f0dd7877b
Djailson1 <djailson_silva1@outlook.com>
2022-06-29 22:26:49 -0300

    Dias, meses, anos e semanas com a nova API Java

--- modulo_datas/src/modulo_datas/DatasEmJava.java
+++ modulo_datas/src/modulo_datas/DatasEmJava.java
@@ -13,18 +13,16 @@ import java.util.Date;
 public class DatasEmJava {
 	public static void main(String[] args) throws ParseException {
 		
-		/*Nova API de data a partir do Java 8*/
+		LocalDate localDate = LocalDate.now();
+		System.out.println("Data atual: " + localDate.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+		
+		System.out.println("Dia da semana: " + localDate.getDayOfWeek().name());
+		System.out.println("Dia do mês: " + localDate.getDayOfMonth());
+		System.out.println("Dia do ano: " + localDate.getDayOfYear());
+		System.out.println("Mês: " + localDate.getMonth());
+		System.out.println("Ano: " + localDate.getYear());
 		
 		
-		LocalDate dataAtual = LocalDate.now();/*como se declara o objeto */
-		System.out.println("Data Atual: " + dataAtual.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
-		/*como imprime e formata*/
-		
-		LocalTime horaAtual = LocalTime.now();
-		System.out.println("Hora atual: " + horaAtual.format(DateTimeFormatter.ofPattern("HH:mm:ss")));
-		
-		LocalDateTime dataAtuaHoraAtual = LocalDateTime.now();
-		System.out.println("Data e hora atual: " + dataAtuaHoraAtual.format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm")));
 		
 		
 		
