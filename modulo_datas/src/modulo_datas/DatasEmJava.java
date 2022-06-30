2dc94f9603fa2ac818d7fc55cec81c9110e500c1
Djailson1 <djailson_silva1@outlook.com>
2022-06-29 23:04:50 -0300

    Faixa de tempo com o objeto periodo

--- modulo_datas/src/modulo_datas/DatasEmJava.java
+++ modulo_datas/src/modulo_datas/DatasEmJava.java
@@ -7,6 +7,7 @@ import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.Calendar;
@@ -14,24 +15,21 @@ import java.util.Date;
 
 public class DatasEmJava {
 	public static void main(String[] args) throws ParseException, InterruptedException {
-		/*para sabermos quanto tempo demora um processo a ser executado*/
-		Instant inicio = Instant.now();
 		
-		Thread.sleep(2000);/*pode ser um processo com tempo qualquer que não conhecemos*/
+		LocalDate dataAntiga = LocalDate.parse("2019-10-05");
+		LocalDate dataNova = LocalDate.parse("2021-10-10");
 		
-		Instant ifinal = Instant.now();
+		System.out.println("Data antiga é maior que data nova? " + dataAntiga.isAfter(dataNova));
+		System.out.println("Data antiga é anterior que data nova? " + dataAntiga.isBefore(dataNova));
+		System.out.println("Datas são iguais? " + dataAntiga.isEqual(dataNova));
 		
-		Duration duration = Duration.between(inicio, ifinal);
-		
-		System.out.println("Duração em nanosegundos: " + duration.toNanos());
-		
-		System.out.println("Duração em minutos: " + duration.toMinutes());
-		
-		System.out.println("Duração em horas: " + duration.toHours());
-		
-		System.out.println("Duração em milisegundos: " + duration.toMillis());
-		
-		
+		Period periodo = Period.between(dataAntiga, dataNova);
+		System.out.println("Quantos dias? " + periodo.getDays());
+		System.out.println("Quantos meses? " + periodo.getMonths());
+		System.out.println("Quantos anos? " + periodo.getYears());
+		System.out.println("Somente meses: " + periodo.toTotalMonths());
+		System.out.println("Período é: " + periodo.getYears() +" anos " +  periodo.getMonths() + 
+				      	   " meses " + periodo.getDays() + " e dias");
 		
 		
 	}
